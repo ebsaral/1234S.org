78dbb694a80b2f5487d431d877cefefa12856829
Emin Saral <eminbugrasaral@me.com>
2025-09-18 21:15:51 +0300

    remove content "theme" from links in footer section

--- website/contents/FooterSection.content.ts
+++ website/contents/FooterSection.content.ts
@@ -37,8 +37,7 @@ const pageContent = {
     }),
     links: {
       githubCommits: "https://github.com/ebsaral/Logical-Spirituality/commits/main/",
-      developer: "https://0.1234s.org",
-      theme: "https://emergent.sh"
+      developer: "https://0.1234s.org"
     }
   },
 } satisfies Dictionary;
