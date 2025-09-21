9d098fd9fa32820f5b5fb2fb248e6a2082b42e3a
Emin Saral <eminbugrasaral@me.com>
2025-09-21 12:13:29 +0300

    add CSP headers into next config

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,8 +1,33 @@
 import { withIntlayer } from "next-intlayer/server";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'self';
+    upgrade-insecure-requests;
+`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: cspHeader.replace(/\n/g, ''),
+                    },
+                ],
+            },
+        ]
+    },
 };
 
 export default withIntlayer(nextConfig);
