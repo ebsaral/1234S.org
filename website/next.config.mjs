4648903a2fcac8b9e6ddf5b66a38db4334bedad8
Emin Saral <eminbugrasaral@me.com>
2025-09-21 12:17:35 +0300

    add vercel.live to script-src

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -2,7 +2,7 @@ import { withIntlayer } from "next-intlayer/server";
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
