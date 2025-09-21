33b90651e7d4e5d32681e5e5216ca57a3c5825ef
Emin Saral <eminbugrasaral@me.com>
2025-09-21 11:11:53 +0300

    add intlayer-config

--- website/intlayer.config.ts
+++ website/intlayer.config.ts
@@ -14,6 +14,32 @@ const config: IntlayerConfig = {
   middleware: {
     prefixDefault: true,
   },
+  editor: {
+    /**
+     * Required
+     * The URL of the application.
+     * This is the URL targeted by the visual editor.
+     */
+    applicationURL: process.env.INTLAYER_APPLICATION_URL,
+    /**
+     * Optional
+     * Default as `8000`.
+     * The port of the editor server.
+     */
+    port: Number(process.env.INTLAYER_PORT || 8000),
+    /**
+     * Optional
+     * Default as "http://localhost:8000"
+     * The URL of the editor server.
+     */
+    editorURL: process.env.INTLAYER_EDITOR_URL,
+    /**
+     * Optional
+     * Default as `true`. If `false`, the editor is inactive and cannot be accessed.
+     * Can be used to disable the editor for specific environments for security reason, such as production.
+     */
+    enabled: Boolean(process.env.INTLAYER_ENABLED || "true")
+  },
   log: {
     mode: "verbose"
   },
