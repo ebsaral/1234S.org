189befdc218a2a9bcace3352d4296d1e590388a9
Emin Saral <eminbugrasaral@me.com>
2025-09-21 11:12:22 +0300

    add securityMiddleware

--- website/src/middleware.ts
+++ website/src/middleware.ts
@@ -1,4 +1,32 @@
-export { intlayerMiddleware as middleware } from "next-intlayer/middleware";
+import { NextRequest } from 'next/server'
+
+import { intlayerMiddleware } from "next-intlayer/middleware";
+import {
+  chain,
+  chainableMiddleware,
+  csp,
+} from "@next-safe/middleware";
+
+const securityMiddleware = csp({
+  // CSP base configuration with IntelliSense
+  // single quotes for values like 'self' are automatic
+  directives: {
+    "default-src": ["self"],
+    "script-src": ["self", "strict-dynamic"],
+    "style-src": ["self", "https://fonts.googleapis.com"],
+    "img-src": ["self"],
+    "font-src": ["self", "https://fonts.gstatic.com"],
+    "object-src": ["none"],
+    "base-uri": ["self"],
+    "form-action": ["self"],
+    "frame-ancestors": ["self"],
+  },
+})
+
+export const middleware = chain(
+  chainableMiddleware((request: NextRequest) => intlayerMiddleware(request)),
+  securityMiddleware
+);
 
 export const config = {
   matcher: [
