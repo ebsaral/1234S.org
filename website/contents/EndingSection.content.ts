c59d3f6a9547e4dbb327a0fcce62c47ca668ef8b
Emin Saral <eminbugrasaral@me.com>
2025-09-21 18:08:39 +0300

    update some section contents and layouts

--- website/contents/EndingSection.content.ts
+++ website/contents/EndingSection.content.ts
@@ -4,8 +4,8 @@ const pageContent = {
   key: "ending-section",
   content: {
     statement: t({
-      en: "In the presence of nature's justice, claiming your rights to equality and freedom can never be questioned.",
-      tr: "Doğadaki adaletin huzurunda, eşitlik ve özgürlük için haklarınızı talep etmeniz asla sorgulanamaz."
+      en: "In the presence of nature's justice, claiming your rights to equality and freedom can never be questioned. Equality exists because no being escapes consequences. Freedom exists because each being chooses its path within them.",
+      tr: "Doğadaki adaletin huzurunda, eşitlik ve özgürlük için haklarınızı talep etmeniz asla sorgulanamaz. Eşitlik, hiçbir varlık sonuçlardan kaçamadığı için vardır. Özgürlük ise her varlığın kendi içindeki yolunu seçebilmesinden doğar."
     })
   },
 } satisfies Dictionary;
