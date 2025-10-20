55cfa3a827d122e323d193f034a969a8eff3aac8
Emin Saral <eminbugrasaral@me.com>
2025-10-20 20:17:21 +0300

    update favicon

--- website/src/app/manifest.ts
+++ website/src/app/manifest.ts
@@ -15,6 +15,21 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: 'any',
         type: 'image/x-icon',
       },
+      {
+        src: '/icon1.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon2.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
     ],
   }
 }
