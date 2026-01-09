5dbdfa173f0908faae88e76ccb89627d224abd63
Emin Saral <eminbugrasaral@me.com>
2026-01-09 20:57:15 +0300

    Update support us button

--- website/src/app/components/Custom/SupportLink.tsx
+++ website/src/app/components/Custom/SupportLink.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { useIntlayer } from "next-intlayer";
+import Link from "next/link";
 
 const SupportLink = ({className}: {className?: string}) => {
     const content = useIntlayer("project-section");
     return (
-    <a
+    <Link
         href={content.sponsorship.href.value}
         className={`text-sm font-medium transition-all duration-300 relative b-1 border-white py-2 px-5 rounded-lg text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 ${className}`}
         >
         {content.sponsorship.support}
-    </a>
+    </Link>
     );
 };
 export default SupportLink;
