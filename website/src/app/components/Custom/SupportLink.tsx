361201e2b7af1f000dfbee23b21f8a212ea88f70
Emin Saral <eminbugrasaral@me.com>
2026-01-12 17:34:08 +0300

    Fix underline border of Support Link

--- website/src/app/components/Custom/SupportLink.tsx
+++ website/src/app/components/Custom/SupportLink.tsx
@@ -12,10 +12,12 @@ const SupportLink = ({className,}: {className?: string}) => {
     return (
     <Link
         href={getLocalizedUrl("/support", locale)}
-        className={`text-sm font-medium transition-all duration-300 relative b-1 border-white py-2 px-5 rounded-lg text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 ${isActive? "underline underline-offset-8 decoration-double ring-white" : ""} ${className}`}
+        className={`flex flex-col items-center justify-center w-auto text-sm font-medium transition-all duration-300 relative b-1 border-white py-2 px-5 rounded-lg text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 ${className}`}
         prefetch={true}
         >
         {content.support.text}
+        
+        {isActive && <span className="absolute bottom-1 left-2 right-2 h-0.5 rounded-full transition-all duration-300 bg-white"></span>}
     </Link>
     );
 };
