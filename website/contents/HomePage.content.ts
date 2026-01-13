8eddaee29d8a291c3cfd58f44fdbc1d74bff6d5c
Emin Saral <eminbugrasaral@me.com>
2026-01-13 22:26:18 +0300

    Update homepage intro

--- website/contents/HomePage.content.ts
+++ website/contents/HomePage.content.ts
@@ -9,7 +9,7 @@ const pageContent = {
     }),
     intro: t({
       en: "Founded with the aim of establishing **Logical Spirituality** as one of the world's leading philosophical movements, embracing the principles of **Interconnectedness, Justice, and Health in Nature** revealed through its teachings. We continue our work to develop, disseminate, and sustain efforts that improve the mental and physical well-being of individuals, society, and the natural environment.",
-      tr: "Dünyanın sayılı felsefi akımlarından biri olmasını amaçladığımız **Mantıksal Maneviyat**'ın öğretileriyle açığa çıkan **Doğada Bütünlük, Adalet ve Sağlık** ilkelerini benimseyerek kuruldu. Bireyin, toplumun ve doğal çevrenin zihinsel ve fiziksel sağlığını iyileştirecek çalışmalarımızı geliştirmeye, yaygınlaştırmaya ve süreklilik kazandırmaya devam ediyoruz.",
+      tr: "Dünyanın sayılı felsefi akımlarından biri olmasını amaçladığımız **Mantıksal Maneviyat**'ın öğretileriyle birlikte açığa çıkan **Doğada Bütünlük, Adalet ve Sağlık** ilkelerini benimseyerek kuruldu. Bireyin, toplumun ve doğal çevrenin zihinsel ve fiziksel sağlığını iyileştirecek çalışmalarımızı geliştirmeye, yaygınlaştırmaya ve süreklilik kazandırmaya devam ediyoruz.",
     }),
     projects: {
       title: t({
