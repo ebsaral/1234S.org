11144154f8982b020e32b2b93cd2f77cd13afeb2
Emin Saral <eminbugrasaral@me.com>
2026-01-17 11:38:27 +0300

    Remove unused imports from philosophy2

--- website/src/app/components/Pages/Philosophy2.tsx
+++ website/src/app/components/Pages/Philosophy2.tsx
@@ -2,10 +2,8 @@ import EndingSection from '@/app/components/Sections/EndingSection';
 import HealthSection from '@/app/components/Sections/HealthSection';
 import Hero from '@/app/components/Sections/Hero';
 import Interconnectedness from '@/app/components/Sections/Interconnectedness';
-import InterconnectednessSection from '@/app/components/Sections/InterconnectednessSection';
 import Intro from '@/app/components/Sections/Intro';
 import JusticeSection from '@/app/components/Sections/JusticeSection';
-import { LocalesValues } from 'intlayer';
 
 const Philosophy = () => {
   return (
