bf76970597ec11457918e9bd4d590b1d146e512c
Emin Saral <eminbugrasaral@me.com>
2026-01-17 11:07:09 +0300

    Set title in Custom Head via document.title

--- website/src/app/components/Custom/Head.tsx
+++ website/src/app/components/Custom/Head.tsx
@@ -3,11 +3,13 @@
 import { DictionaryRegistry } from '@intlayer/types';
 import { LocalesValues } from 'intlayer';
 import { useIntlayer } from 'next-intlayer';
-import NextHead from 'next/head';
 
 const Head = ({ dict, locale }: { dict: keyof DictionaryRegistry; locale: LocalesValues }) => {
   const content = useIntlayer(dict, locale);
-  return <NextHead>{'title' in content && <title>{(content as any).title}</title>}</NextHead>;
+  if ('title' in content) {
+    document.title = (content as any).title.value;
+  }
+  return <></>;
 };
 
 export default Head;
