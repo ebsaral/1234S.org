62c49eeceff64d50e771846425cdaa68d6f278cb
Emin Saral <eminbugrasaral@me.com>
2026-02-01 19:03:26 +0300

    Update team member description

--- website/contents/Sections/Team.content.ts
+++ website/contents/Sections/Team.content.ts
@@ -69,8 +69,8 @@ const pageContent = {
           tr: 'İşsiz',
         }),
         description: t({
-          en: "One of the ordinary citizens who, having had to struggle with various illnesses for years, drew closer to the Creator before Emin Buğra Saral. He continues to battle with some of his mental health issues in his simple and boring life. His existence has enormously contributed to his brother's marvellous discoveries.",
-          tr: "Çeşitli hastalıklarla yıllarca mücadele etmek zorunda bırakılan, Yaradan'a Emin Buğra Saral'dan daha önce yaklaşan, sıradan vatandaşlardan biri. Sade ve sıkıcı olan hayatında  zihinsel hastalıklarının bir kısmıyla uğraşmaya devam ediyor. Abisinin olağanüstü keşiflerine varlığıyla birçok katkıda bulunmuştur.",
+          en: "One of the ordinary citizens who, having had to struggle with various illnesses for years, drew closer to the Creator before Emin Buğra Saral. He continues to battle with some of his mental health issues in his simple and boring life. He is married and has five kids. His existence has enormously contributed to his brother's marvellous discoveries.",
+          tr: "Çeşitli hastalıklarla yıllarca mücadele etmek zorunda bırakılan, Yaradan'a Emin Buğra Saral'dan daha önce yaklaşan, sıradan vatandaşlardan biri. Sade ve sıkıcı olan hayatında  zihinsel hastalıklarının bir kısmıyla uğraşmaya devam ediyor. Evli ve beş çocuk babasıdır. Abisinin olağanüstü keşiflerine varlığıyla birçok katkıda bulunmuştur. ",
         }),
         birthday: {
           year: 1990,
