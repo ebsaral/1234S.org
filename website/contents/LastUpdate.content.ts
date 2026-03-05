54cfc035f40525c3c9dc6014489f494117232903
Emin Saral <eminbugrasaral@me.com>
2026-03-05 09:09:09 +0300

    Update explanations in Health section

--- website/contents/LastUpdate.content.ts
+++ website/contents/LastUpdate.content.ts
@@ -3,16 +3,18 @@ import { insert, md, t, type Dictionary } from 'intlayer';
 const pageContent = {
   key: 'last-update',
   content: {
-    date: '2026-03-04',
+    date: '2026-03-05',
     update: t({
       en: insert('Last content update: {{date}}'),
       tr: insert('Son içerik güncelleme: {{date}}'),
     }),
     reason: t({
       en: md(
-        'A new paragraph about the language of nature waiting to be understood is added to Justice in Nature section.',
+        'The explanations about the journey of harmful substances in our body and punishments in nature are updated.',
       ),
-      tr: md('Doğadaki Adalet kısmına, doğanın anlaşılmayı bekleyen diline dair yeni bir paragraf eklendi.'),
+      tr: md(
+        'Sağlık kısmındaki zararlı maddelerin vücudumuzdaki yolculuğuna ve doğadaki cezalandırmaya yönelik açıklamalar güncellendi.',
+      ),
     }),
   },
 } satisfies Dictionary;
