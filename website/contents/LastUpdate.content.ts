92560c0264bf7af8b310faadf7d488988c58ba8a
Emin Saral <eminbugrasaral@me.com>
2026-03-13 19:26:28 +0300

    Update an answer in FAQ section

--- website/contents/LastUpdate.content.ts
+++ website/contents/LastUpdate.content.ts
@@ -3,14 +3,16 @@ import { insert, md, t, type Dictionary } from 'intlayer';
 const pageContent = {
   key: 'last-update',
   content: {
-    date: '2026-03-11',
+    date: '2026-03-13',
     update: t({
       en: insert('Last content update: {{date}}'),
       tr: insert('Son içerik güncelleme: {{date}}'),
     }),
     reason: t({
-      en: md('The explanation in Organ-Action-Result Analogy in Health section is updated.'),
-      tr: md("Sağlık kısmındaki Organ-Eylem-Sonuç Benzetmesi'ndeki açıklama güncellendi."),
+      en: md(
+        'An addition has been made to the answer to the fifth question in the Frequently Asked Questions section.',
+      ),
+      tr: md("Sıkça Sorulan Sorular'daki beşinci sorunun cevabına ekleme yapıldı."),
     }),
   },
 } satisfies Dictionary;
