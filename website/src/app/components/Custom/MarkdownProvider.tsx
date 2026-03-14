83f41bc1f41e00c80fe6f07933dc6c685e6ae81e
Emin Saral <eminbugrasaral@me.com>
2026-03-14 15:24:31 +0300

    Add Markdown Provider

---
+++ website/src/app/components/Custom/MarkdownProvider.tsx
@@ -0,0 +1,27 @@
+import { MarkdownProvider } from 'react-intlayer';
+import Markdown from 'react-markdown';
+import remarkAttrs from 'remark-attrs';
+import remarkGfm from 'remark-gfm';
+import CustomMarkdownLink from '../Custom/CustomMarkdownLink';
+
+const MP = ({ children }: React.PropsWithChildren) => {
+  const components = {
+    a: CustomMarkdownLink,
+  };
+
+  return (
+    <MarkdownProvider
+      renderMarkdown={(markdown) => (
+        <>
+          <Markdown remarkPlugins={[remarkGfm, remarkAttrs]} components={components}>
+            {markdown}
+          </Markdown>
+        </>
+      )}
+    >
+      {children}
+    </MarkdownProvider>
+  );
+};
+
+export default MP;
