ad53ba196add06868866052b08fd75aaabd39e2d
Emin Buğra Saral <eminbugrasaral@me.com>
2026-03-15 22:23:01 +0300

    Add scroll to top at click event on logo in Header section

--- website/src/app/components/Header.jsx
+++ website/src/app/components/Header.jsx
@@ -133,7 +133,7 @@ const Header = ({ postCount }) => {
             href={getLocalizedUrl('/', locale)}
             aria-label={getItemTitle('home')}
             title={getItemTitle('home')}
-            onClick={() => setIsMenuOpen(false)}
+            onClick={() => scrollToSection('#scroll-to-top')}
             className='group flex flex-row justify-end items-center rounded-md cursor-pointer transition-colors duration-300'
             prefetch={true}
           >
