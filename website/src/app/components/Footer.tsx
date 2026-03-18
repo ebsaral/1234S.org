7a36374245283a54b63c3edf7a60162674b9b108
Emin Saral <eminbugrasaral@me.com>
2026-03-18 13:21:29 +0300

    Update icons and styling in Footer section

--- website/src/app/components/Footer.tsx
+++ website/src/app/components/Footer.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import { useIntlayer } from 'next-intlayer';
+import { FaGithub, FaProductHunt } from 'react-icons/fa';
+import { FaLinkedin, FaSquareXTwitter } from 'react-icons/fa6';
+
 import { SocialIcon } from 'react-social-icons';
 import MarkdownProvider from './Custom/MarkdownProvider';
 
@@ -14,53 +17,69 @@ const Footer = () => {
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
           <div className='max-w-7xl flex flex-col justify-center items-center gap-8'>
             {/* Social Media section */}
-            <div id='social-links' className='flex flex-col gap-8 items-center justify-center mt-4'>
+            <div className='flex flex-col gap-8 items-center justify-center mt-4'>
               <div className='flex justify-center items-center gap-4'>
-                {content.social.map((props, index) => (
-                  <SocialIcon
-                    key={index}
-                    title={props.label.value}
-                    target='_blank'
-                    className='transition-all duration-300 transform hover:scale-110'
-                    url={props.href.value}
-                  />
-                ))}
+                {/* X (Twitter) */}
+                <a
+                  className='hover:scale-110 transition-all duration-300 text-gray-100 opacity-90 hover:opacity-100'
+                  href={content.social[0].href.value}
+                  title={content.social[0].label.value}
+                  aria-label={content.social[0].label.value}
+                >
+                  <FaSquareXTwitter size={36} />
+                </a>
+
+                {/* LinkedIn */}
+                <a
+                  className='hover:scale-110 transition-all duration-300 text-gray-100 opacity-90 hover:opacity-100'
+                  href={content.social[1].href.value}
+                  title={content.social[1].label.value}
+                  aria-label={content.social[1].label.value}
+                >
+                  <FaLinkedin size={36} />
+                </a>
               </div>
             </div>
 
-            <div className='inline-flex items-center justify-center gap-3 sm:gap-5'>
+            <div className='flex flex-col items-center justify-center gap-3 sm:gap-5'>
               <p className='text-center text-gray-300'>
                 <em>{content.note}</em>
+              </p>
+              <p className='text-sm font-medium link-underline hover:no-underline'>
+                <a className='text-gray-100' href={content.contact.href.value}>
+                  {content.contact.label}
+                </a>
               </p>
             </div>
 
             <div className='flex flex-col gap-3 items-center justify-center text-gray-300 text-xs whitespace-pre-line'>
-              <div className='flex flex-col items-center justify-center gap-3 sm:gap-5'>
-                <p className='text-sm font-medium'>
+              <div className='link-underline text-center text-xs'>{content.interconnectedness}</div>
+
+              <div className='flex flex-row items-center justify-center gap-3 sm:gap-5'>
+                <p className='link-underline text-center'>
                   <a
-                    className='text-gray-100 hover:text-gray-200 link-underline hover:no-underline'
-                    href={content.contact.href.value}
+                    className='text-gray-100 opacity-90 hover:opacity-100'
+                    href={content.github.href.value}
+                    title={content.github.label.value}
+                    aria-label={content.github.label.value}
                   >
-                    {content.contact.label}
+                    <FaGithub size={24} />
                   </a>
                 </p>
+                <p className='link-underline text-center'>
+                  <a
+                    className='text-gray-100 opacity-90 hover:opacity-100'
+                    href={content.productHunt.href.value}
+                    title={content.productHunt.label.value}
+                    aria-label={content.productHunt.label.value}
+                  >
+                    <FaProductHunt size={24} />
+                  </a>
+                </p>
+              </div>
+              <div className='flex flex-col items-center justify-center gap-3 sm:gap-5'>
                 <p>
                   {new Date().getFullYear()} © {content.name}
-                </p>
-              </div>
-              <div className='text-center [&_a]:underline [&_a]:hover:no-underline text-xs'>
-                {content.interconnectedness}
-              </div>
-              <div className='flex flex-row items-center justify-center gap-3 sm:gap-5'>
-                <p className='text-center'>
-                  <a className='link-underline hover:no-underline' href={content.github.href.value}>
-                    {content.github.label}
-                  </a>
-                </p>
-                <p className='text-center'>
-                  <a className='link-underline hover:no-underline' href={content.productHunt.href.value}>
-                    {content.productHunt.label}
-                  </a>
                 </p>
               </div>
             </div>
