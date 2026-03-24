60812f6a937d8fed7eb15a2f371a437ff7c22747
Emin Saral <eminbugrasaral@me.com>
2026-03-24 12:11:43 +0300

    Add a new information about accidents to Health section

--- website/contents/LastUpdate.content.ts
+++ website/contents/LastUpdate.content.ts
@@ -3,14 +3,14 @@ import { insert, md, t, type Dictionary } from 'intlayer';
 const pageContent = {
   key: 'last-update',
   content: {
-    date: '2026-03-22',
+    date: '2026-03-24',
     update: t({
       en: insert('Last content update: {{date}}'),
       tr: insert('Son içerik güncelleme: {{date}}'),
     }),
     reason: t({
-      en: md('The description in introduction section is updated.'),
-      tr: md('Giriş kısmındaki tanımlama güncellendi.'),
+      en: md('An information about accidents is added to Health section.'),
+      tr: md('Kazalar hakkında bir bilgi Sağlık kısmına eklendi.'),
     }),
   },
 } satisfies Dictionary;
