b61197c57053b5e665c91044860ac8140c2390f4
Emin Saral <eminbugrasaral@me.com>
2026-03-27 02:24:16 +0300

    Update testimonials

--- website/contents/Testimonials.content.ts
+++ website/contents/Testimonials.content.ts
@@ -11,32 +11,12 @@ const pageContent = {
     items: [
       {
         name: t({
-          en: 'An Atheist',
-          tr: 'Bir Ateist',
-        }),
-        text: t({
-          en: 'I have read somewhere before that God exists, but no one had ever explained how it does. I’m in shock!',
-          tr: 'Tanrının var olduğunu daha önce bir yerlerde okumuştum, fakat hiç kimse nasıl var olduğunu anlatmamıştı. Şoktayım!',
-        }),
-      },
-      {
-        name: t({
           en: 'A Disabled',
           tr: 'Bir Engelli',
         }),
         text: t({
-          en: 'If I was raised with these teachings here, I could have not lost my other leg. Damn it!',
-          tr: 'Eğer buradaki öğretilere göre yetiştirilmiş olsaydım, diğer bacağımı kaybetmemiş olabilirdim. Lanet olsun!',
-        }),
-      },
-      {
-        name: t({
-          en: 'A Religion Officer',
-          tr: 'Bir Din Görevlisi',
-        }),
-        text: t({
-          en: "It means that all the people we’ve listened to, by saying 'the command comes from above,' were just ordinary servants like us.",
-          tr: "Demek ki, 'emir büyük yerden' diyerek dinlediğimiz insanların hepsi, bizim gibi sıradan kullardı.",
+          en: 'If I had been raised with these teachings here, I might not have lost my leg. Damn it!',
+          tr: 'Eğer buradaki öğretilere göre yetiştirilmiş olsaydım, bacağımı kaybetmeyebilirdim. Lanet olsun!',
         }),
       },
       {
@@ -51,16 +31,6 @@ const pageContent = {
       },
       {
         name: t({
-          en: 'A Professor',
-          tr: 'Bir Profesör',
-        }),
-        text: t({
-          en: 'After receiving all those awards and diplomas, seeing what I thought I knew is actually what I don’t know hurts a bit.',
-          tr: 'Aldığım o kadar ödülden ve diplomadan sonra bildiklerimin aslında bilmediklerim olduğunu görmem biraz acıtmadı değil.',
-        }),
-      },
-      {
-        name: t({
           en: 'A Soldier',
           tr: 'Bir Asker',
         }),
@@ -71,12 +41,42 @@ const pageContent = {
       },
       {
         name: t({
+          en: 'A Religion Officer',
+          tr: 'Bir Din Görevlisi',
+        }),
+        text: t({
+          en: "So all the people we’ve followed by saying 'the command comes from above' were ordinary servants like us.",
+          tr: "Demek ki, 'emir büyük yerden' diyerek dinlediğimiz insanların tamamı bizim gibi sıradan kullardı.",
+        }),
+      },
+      {
+        name: t({
+          en: 'A Professor',
+          tr: 'Bir Profesör',
+        }),
+        text: t({
+          en: 'After receiving all those diplomas and awards, seeing what I thought I knew is actually what I don’t know hurts a bit.',
+          tr: 'Aldığım o kadar diplomadan ve ödülden sonra bildiklerimin aslında bilmediklerim olduğunu görmem biraz acıtmadı değil.',
+        }),
+      },
+      {
+        name: t({
           en: 'An Ordinary Citizen',
           tr: 'Sıradan Bir Vatandaş',
         }),
         text: t({
           en: 'I want to live in a system where we can no longer be fooled. Let’s hope for the best!',
-          tr: 'Artık kandırılamayacağımız bir sistemde yaşamak istiyorum. Hakkımızda hayırlısı olsun.',
+          tr: 'Artık kandırılamayacağımız bir sistemde yaşamak istiyorum. Hakkımızda hayırlısı olsun!',
+        }),
+      },
+      {
+        name: t({
+          en: 'An Atheist',
+          tr: 'Bir Ateist',
+        }),
+        text: t({
+          en: 'I have read somewhere before that God exists, but no one had ever explained how it does. I’m in shock!',
+          tr: 'Tanrının var olduğunu daha önce bir yerlerde okumuştum, fakat hiç kimse nasıl var olduğunu anlatmamıştı. Şoktayım!',
         }),
       },
       {
@@ -85,18 +85,18 @@ const pageContent = {
           tr: 'Bir İş İnsanı',
         }),
         text: t({
-          en: 'I was already struggling to understand why we are working so much. It turns out we need more practical and healthy solutions.',
-          tr: 'Neden bu kadar fazla çalıştığımızı anlamakta zorlanıyordum zaten. Daha pratik ve sağlıklı çözümlerin bulunması gerekiyormuş.',
+          en: 'I was struggling to understand why we are working so much. It turns out we need more practical and healthy solutions.',
+          tr: 'Neden bu kadar fazla çalıştığımızı anlamakta zorlanıyordum. Daha pratik ve sağlıklı çözümlerin bulunması gerekiyormuş.',
         }),
       },
       {
         name: t({
-          en: 'A Marketting Person',
+          en: 'A Sales Person',
           tr: 'Bir Pazarlamacı',
         }),
         text: t({
           en: 'Is it okay if I don’t comment on this?',
-          tr: 'Ben yorum yapmasam olur mu ya?',
+          tr: 'Ben yorum yapmasam olur mu acaba?',
         }),
       },
       {
@@ -105,8 +105,8 @@ const pageContent = {
           tr: 'Bir Avukat',
         }),
         text: t({
-          en: 'The fact that the entire system actually protects the rich is not fair at all. We need to be able to access equal opportunities easily.',
-          tr: 'Tüm sistemin aslında zenginleri koruyor olması hiç adil değil. Eşit fırsatlara kolayca erişebilmemiz gerekiyor.',
+          en: 'The fact that the entire system actually protects the rich is not fair. I like seeing that we can access equal opportunities easily.',
+          tr: 'Tüm sistemin aslında zenginleri koruyor olması hiç adil değil. Eşit fırsatlara kolayca erişebileceğimizi görmek hoşuma gitti.',
         }),
       },
       {
@@ -116,7 +116,7 @@ const pageContent = {
         }),
         text: t({
           en: 'Honestly, being able to see that we’re not much different from the other citizens is quite eye-opening.',
-          tr: 'Diğer vatandaşlardan pek bir farkımızın olmadığını görebilmek epey ufuk açıcı oldu açıkçası.',
+          tr: 'Açıkçası, diğer vatandaşlardan pek bir farkımızın olmadığını görebilmek epey ufuk açıcı oldu.',
         }),
       },
       {
