6427912705d7898bc126a2855681be1c2847144e
Emin Saral <eminbugrasaral@me.com>
2026-04-01 17:30:22 +0300

    Update last update note

--- website/contents/LastUpdate.content.ts
+++ website/contents/LastUpdate.content.ts
@@ -3,14 +3,18 @@ import { insert, md, t, type Dictionary } from 'intlayer';
 const pageContent = {
   key: 'last-update',
   content: {
-    date: '2026-03-27',
+    date: '2026-04-01',
     update: t({
       en: insert('Last content update: {{date}}'),
       tr: insert('Son içerik güncelleme: {{date}}'),
     }),
     reason: t({
-      en: md('The description text in Introduction section is updated.'),
-      tr: md('Giriş kısmındaki açıklama metni güncellendi.'),
+      en: md(
+        "A new question has been added to the Frequently Asked Questions section, and an update has been made to the answer to the question 'Where might we be going?'",
+      ),
+      tr: md(
+        "Sıkça Sorulan Sorular kısmına yeni bir soru eklendi ve 'Nereye Gidiyor Olabiliriz?' sorusunun cevabına ekleme yapıldı.",
+      ),
     }),
   },
 } satisfies Dictionary;
