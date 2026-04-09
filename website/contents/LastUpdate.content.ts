59c0a4680590ca4d8c79522c9e9502a84ca6432f
Emin Saral <eminbugrasaral@me.com>
2026-04-10 01:52:37 +0300

    Update some questions and answers in Frequently Asked Questions section

--- website/contents/LastUpdate.content.ts
+++ website/contents/LastUpdate.content.ts
@@ -3,18 +3,16 @@ import { insert, md, t, type Dictionary } from 'intlayer';
 const pageContent = {
   key: 'last-update',
   content: {
-    date: '2026-04-01',
+    date: '2026-04-10',
     update: t({
       en: insert('Last content update: {{date}}'),
       tr: insert('Son içerik güncelleme: {{date}}'),
     }),
     reason: t({
       en: md(
-        "A new question has been added to the Frequently Asked Questions section, and an update has been made to the answer to the question 'Where might we be going?'",
+        'A new question has been added to the Frequently Asked Questions section, and answers to some questions have been expanded.',
       ),
-      tr: md(
-        "Sıkça Sorulan Sorular kısmına yeni bir soru eklendi ve 'Nereye Gidiyor Olabiliriz?' sorusunun cevabına ekleme yapıldı.",
-      ),
+      tr: md('Sıkça Sorulan Sorular kısmına yeni bir soru eklendi ve bazı soruların cevapları genişletildi.'),
     }),
   },
 } satisfies Dictionary;
